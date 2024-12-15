50c1b3aefade0682736f65b5c744a10cdc207ae7
iteng007 <iteng0718@gmail.com>
2024-12-15 13:41:43 +0000

    Enhance install.sh to download additional configuration files for Vite and TypeScript setup

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ printf \"\\033[B\\033[B\\n\" | npm create --yes vite@latest . -- --template reac
   npm install -D @types/node
   npx --yes shadcn@latest init
   npx --yes shadcn@latest add button
-
+curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/vite.config.ts -o vite.config.ts --create-dirs -f
+curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tsconfig.json -o tsconfig.json --create-dirs -f
+curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tsconfig.app.json -o tsconfig.app.json --create-dirs -f
+curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tailwind.config.js -o tailwind.config.js --create-dirs -f
+curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/src/index.css -o src/index.css --create-dirs -f
