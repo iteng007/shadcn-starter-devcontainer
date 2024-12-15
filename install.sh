a7fd3903c553ad24ea03c609b31957076bd1264d
iteng007 <iteng0718@gmail.com>
2024-12-15 13:43:04 +0000

    Refactor install.sh to remove duplicate file downloads and streamline setup process for Vite and TypeScript configurations

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ printf \"\\033[B\\033[B\\n\" | npm create --yes vite@latest . -- --template reac
   npx tailwindcss init -p && \
   npm install -y
   npm install -D @types/node
+  curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/vite.config.ts -o vite.config.ts --create-dirs -f
+  curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tsconfig.json -o tsconfig.json --create-dirs -f
+  curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tsconfig.app.json -o tsconfig.app.json --create-dirs -f
+  curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tailwind.config.js -o tailwind.config.js --create-dirs -f
+  curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/src/index.css -o src/index.css --create-dirs -f
   npx --yes shadcn@latest init
   npx --yes shadcn@latest add button
-curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/vite.config.ts -o vite.config.ts --create-dirs -f
-curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tsconfig.json -o tsconfig.json --create-dirs -f
-curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tsconfig.app.json -o tsconfig.app.json --create-dirs -f
-curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/tailwind.config.js -o tailwind.config.js --create-dirs -f
-curl -f https://raw.githubusercontent.com/iteng007/shadcn-starter-devcontainer/refs/heads/main/src/index.css -o src/index.css --create-dirs -f
+
